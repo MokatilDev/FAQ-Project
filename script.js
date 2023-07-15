9b86d427019ecdf20b76adf899c1d5996dbbd2e2
MokatilDev <mokatilgfx@gmail.com>
2023-07-15 16:42:26 +0100

    Add CopyRight

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const Question = document.querySelectorAll(".Question")
 const Anwsor = document.querySelector(".Anwsor")
+const Question1 = document.querySelector(".Question1")
 
 
-
-
-
+let x = 2
 Question.forEach(element =>{
     element.addEventListener("click",()=>{
         element.children[1].classList.toggle("active")
         element.nextElementSibling.classList.toggle("active")
     })
+})
+
+Question1.addEventListener("click",()=>{
+    if(x%2 == 0){
+        Question1.style.borderBottom = "1px solid var(--Boder)"
+    }else{
+        Question1.style.borderBottom = "0"
+    }
+    x++
 })
